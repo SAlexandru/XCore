1fd607a5a81988d28ce69b5bfb4f9ed2ef89f004
Petru Florin Mihancea <petrum@gmail.com>
2015-10-24 10:08:29 +0300

    simplified open action in insider

--- XCorexView/src/ro/lrg/insider/view/BrowseEntityAction.java
+++ XCorexView/src/ro/lrg/insider/view/BrowseEntityAction.java
@@ -1,8 +1,5 @@
 package ro.lrg.insider.view;
 
-import java.util.ArrayList;
-
-import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -12,50 +9,25 @@ import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 
-import com.salexandru.xcore.interfaces.XEntity;
-
 public class BrowseEntityAction implements IObjectActionDelegate {
 
 	private ISelection selection_;
-		
-	public interface XEntityConverter {
-		public XEntity convert(IJavaElement element);
-	}
-	
-	private static ArrayList<XEntityConverter> converters = new ArrayList<>();
-	
-	private static XEntity toXEntity(IJavaElement element) {
-		for(XEntityConverter aConv : converters) {
-			XEntity res = aConv.convert(element);
-			if(res != null) {
-				return res;
-			}
-		}
-		return null;
-	}
-	
-	public static void registerXEntityConverter(XEntityConverter conv) {
-		converters.add(conv);
-	}
-	
+			
 	@Override
 	public void run(IAction action) {
-		IJavaElement element = ((IJavaElement)((IStructuredSelection)selection_).getFirstElement());	
-		XEntity entity = toXEntity(element);			
-		if(entity == null) {
-			return;
-		}
 
-		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
 		try {
+
+			IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
 			page.showView(XCorexTableView.viewId);
+			XCorexTableView view = (XCorexTableView)page.findView(XCorexTableView.viewId);
+			view.displayEntity(((IStructuredSelection)selection_).getFirstElement());
+
 		} catch (PartInitException e) {
 			e.printStackTrace();
 			return;
 		}
-						
-		XCorexTableView view = (XCorexTableView)page.findView(XCorexTableView.viewId);
-		view.displayEntity(entity);
+		
 	}
 	
 	@Override
