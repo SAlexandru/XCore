48bff9ea1dcad8fdaa1f67b16099ed1e31969044
SAlexandru <salexandru.alex@gmail.com>
2017-06-02 21:19:15 +0300

    added the introduction part

---
+++ Docs/Thesis/Dissertation/src/compile.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+pdflatex main.tex
+bibtex main
+makeglossaries main
+pdflatex main.tex
+pdflatex main.tex
+pdflatex -halt-on-error main.tex
+
+rm -rf main.aux  main.bbl  main.blg  main.lof  main.log  main.lot  main.toc
+mv main.pdf output/main.pdf
+
+open output/main.pdf
