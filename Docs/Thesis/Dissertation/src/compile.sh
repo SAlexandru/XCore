173cf98bdbc7e8b695a861b8570dc6680448756a
SAlexandru <salexandru.alex@gmail.com>
2017-06-07 21:26:51 +0300

    almost done

--- Docs/Thesis/Dissertation/src/compile.sh
+++ Docs/Thesis/Dissertation/src/compile.sh
@@ -7,7 +7,7 @@ pdflatex main.tex
 pdflatex main.tex
 pdflatex -halt-on-error main.tex
 
-rm -rf main.aux  main.bbl  main.blg  main.lof  main.log  main.lot  main.toc
+rm -rf main.aux  main.bbl  main.blg  main.lof  main.log  main.lot  main.toc main.out
 mv main.pdf output/main.pdf
 
 open output/main.pdf
