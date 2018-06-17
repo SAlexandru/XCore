39cdee51819e36cdafb1b41ceec120a41a6a8bc8
Petru Florin Mihancea <petrum@gmail.com>
2018-06-17 21:18:41 +0300

    make bindings persistent in a file that can then be shared via a versioning system

--- XCore/src/com/salexandru/xcore/preferencepage/XCorexPropertyPage.java
+++ XCore/src/com/salexandru/xcore/preferencepage/XCorexPropertyPage.java
@@ -2,18 +2,23 @@ package com.salexandru.xcore.preferencepage;
 
 import java.util.Set;
 
-import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.resources.ProjectScope;
 import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.core.runtime.QualifiedName;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.IScopeContext;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.PreferenceStore;
 import org.eclipse.ui.IWorkbenchPropertyPage;
+import org.osgi.service.prefs.BackingStoreException;
 
+import com.salexandru.xcore.plugin.Activator;
 import com.salexandru.xcore.utils.interfaces.XEntity;
 
 public class XCorexPropertyPage extends FieldEditorPreferencePage implements IWorkbenchPropertyPage {
 	
+	private static final String PROP_KEY = "bindings";
+
 	private IJavaProject theProject;
 	
 	public XCorexPropertyPage() {
@@ -37,25 +42,32 @@ public class XCorexPropertyPage extends FieldEditorPreferencePage implements IWo
 	}
 
 	public static class XCorePropertyStore extends PreferenceStore {
+		
 		private IJavaProject prj;
-		private String pageId = "com.salexandru.xcore.XCorePropertyPage";
+		
 		public XCorePropertyStore(IJavaProject prj) {
 			this.prj = prj;
 			load();
 		}
+		
 		public void save() {}
+		
 		public void doSave() {
 			try {
 				String[] allNames = super.preferenceNames();
 				String value = "";
 				for(String aName : allNames) {
-					value+= aName + "," + super.getString(aName) + ";";
+					value += aName + "," + super.getString(aName) + ";";
 				}
-				prj.getResource().setPersistentProperty(new QualifiedName(pageId, "XCore bindings definition"), value);
-			} catch (CoreException e) {
+				IScopeContext projectScope = new ProjectScope(prj.getProject());
+				IEclipsePreferences projectNode = projectScope.getNode(Activator.PLUGIN_ID);
+				projectNode.put(PROP_KEY, value);
+				projectNode.flush();
+			} catch (BackingStoreException e) {
 				e.printStackTrace();
 			}
 		}
+		
 		public void doSave(Set<String> toRemove) {
 			try {
 				String[] allNames = super.preferenceNames();
@@ -63,15 +75,21 @@ public class XCorexPropertyPage extends FieldEditorPreferencePage implements IWo
 				for(String aName : allNames) {
 					if(!toRemove.contains(aName))
 						value+= aName + "," + super.getString(aName) + ";";
+					
 				}
-				prj.getResource().setPersistentProperty(new QualifiedName(pageId, "XCore bindings definition"), value);
-			} catch (CoreException e) {
+				IScopeContext projectScope = new ProjectScope(prj.getProject());
+				IEclipsePreferences projectNode = projectScope.getNode(Activator.PLUGIN_ID);
+				projectNode.put(PROP_KEY, value);
+				projectNode.flush();
+			} catch (BackingStoreException e) {
 				e.printStackTrace();
 			}
 		}
+		
 		public void load() {
-			try {
-				String value = prj.getResource().getPersistentProperty(new QualifiedName(pageId, "XCore bindings definition"));
+				IScopeContext projectScope = new ProjectScope(prj.getProject());
+				IEclipsePreferences projectNode = projectScope.getNode(Activator.PLUGIN_ID);
+				String value = projectNode.get(PROP_KEY,null);
 				if(value == null || value.equals("")) return;
 				String[] allEntries = value.split(";");
 				for(String anEntry : allEntries) {
@@ -79,18 +97,18 @@ public class XCorexPropertyPage extends FieldEditorPreferencePage implements IWo
 					super.setValue(values[0],values[1] + "," + values[2]);
 					super.setDefault(values[0], Object.class.getCanonicalName() + "," + XEntity.class.getCanonicalName());
 				}
-			} catch (CoreException e) {
-				e.printStackTrace();
-			}
 		}
+		
 		public void setBindings(String entity, String underlyingType, String upLinkType) {
 			super.setValue(entity, underlyingType + "," + upLinkType);
 			super.setDefault(entity, Object.class.getCanonicalName() + "," + XEntity.class.getCanonicalName());
 		}
+		
 		public void setDefaultBindings(String entity) {
 			super.setDefault(entity, Object.class.getCanonicalName() + "," + XEntity.class.getCanonicalName());
 			super.setToDefault(entity);
 		}
+		
 		public String getExtendedMetaType(String entity) {
 			String tmp = super.getString(entity);
 			return !tmp.equals("") ? tmp.substring(tmp.indexOf(",") + 1) : null;
@@ -111,6 +129,7 @@ public class XCorexPropertyPage extends FieldEditorPreferencePage implements IWo
 			}
 			return res;
 		}
+		
 		public IJavaProject getProject() {
 			return prj;
 		}
